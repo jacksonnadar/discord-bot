b7eee5552eb003ae72f68e5529e409ba9580e3cd
jackson <jackson@pop-os.localdomain>
2020-08-25 21:49:56 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { Client } = require('discord.js');
+
+const WORDS = require('./words');
+
+const client = new Client();
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+// monty 534629359810772993
+// jack 431490737683824661
+
+client.on('message', (msg) => {
+  console.log(msg.author);
+  if (msg.author.id == '534629359810772993') {
+    msg.reply(chooseRandomSentence());
+  }
+});
+
+function chooseRandomSentence() {
+  const random_no = Math.floor(Math.random() * WORDS.monty.length);
+  return WORDS.monty[random_no];
+}
---
+++ src/words.js
@@ -0,0 +1,14 @@
+const WORDS = {
+  monty: [
+    'abe lavde tea utha b hai kya',
+    'chup rand k bachhe',
+    'ja an*ali po patta ne ki try kar',
+    'kidar ja k apni gand mara bosdk',
+    'panta nai tu bot se q gali ka raha hai benchod',
+    'why are you gay',
+    'bag bosdk',
+    'chake ki aulad badva sala',
+  ],
+};
+
+module.exports = WORDS;
